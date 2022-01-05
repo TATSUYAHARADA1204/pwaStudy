e16e074606674c7e26c6f74639cd861accb625c9
TATSUYAHARADA1204 <demitas2@gmail.com>
2022-01-05 21:53:39 +0900

    fix navigator serect

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,20 +1,11 @@
 // service worker 
 if('serviceWorker' in navigator){
-    window.addEventListener('load',()=> {
-        navigator.serviceWorker.register('service_worker.js')
-            .then(reg=> {
-              console.log('サービスワーカーの登録が以下のスコープで完了'.reg.scope)  
-            })
-            .catch(err => {
-                return console.log('サービスワーカーの登録が失敗:',err);
-            });
-    });
-//    navigator.serviceWorker.register('service_worker.js')
-        //.then(function(registration){
-            //console.log('ServiceWoeker registration successful with scope: ',registration.scope);
-        //}).catch(function(err){
-            //console.log('ServiceWoeker registration failed: ',err);
-        //});
+    navigator.serviceWorker.register('service_worker.js')
+        .then(function(registration){
+            console.log('ServiceWoeker registration successful with scope: ',registration.scope);
+        }).catch(function(err){
+            console.log('ServiceWoeker registration failed: ',err);
+        });
 }
 
 var num = 0;
